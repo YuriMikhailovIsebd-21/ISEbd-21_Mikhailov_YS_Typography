08e77cbb884ad4ebee0a0b6b4bd8284290750788
Юрий Михайлов <yuri_2002_q@mail.ru>
2022-04-22 19:01:00 +0400

    Lab4

---
+++ Typography/TypographyContracts/ViewModels/ReportOrdersViewModel.cs
@@ -0,0 +1,18 @@
+﻿using TypographyContracts.Enums;
+using System;
+
+namespace TypographyContracts.ViewModels
+{
+    public class ReportOrdersViewModel
+    {
+        public DateTime DateCreate { get; set; }
+
+        public string PrintedName { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Sum { get; set; }
+
+        public string Status { get; set; }
+    }
+}
